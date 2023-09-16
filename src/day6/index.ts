051ee7aae0ca3ea6156407dcd7225f2362ad7a7a
Jonathan Liera <jonliera1@gmail.com>
2023-09-16 09:47:15 -0700

    functional programming variation part 1 day 6

--- src/day6/index.ts
+++ src/day6/index.ts
@@ -5,22 +5,34 @@ const rl = readline.createInterface({
   input: fs.createReadStream("./input.txt"),
 });
 
-function isUnique(array: any[]) {
+function isUnique(array: string[]) {
   return array.length === new Set(array).size;
 }
-
 let fourElements: string[] = [];
+// let count: number | undefined = undefined;
 rl.on("line", (line) => {
-  for (let i = 0; i < line.length; i++) {
-    fourElements.push(line[i]);
+  line.split("").every((char, i) => {
+    fourElements.push(char);
     if (fourElements.length > 4) {
       fourElements.shift();
     }
     if (fourElements.length === 4 && isUnique(fourElements)) {
       console.log(i + 1);
-      break;
+      return false;
     }
-  }
+    return true;
+  });
+
+  // for (let i = 0; i < line.length; i++) {
+  //   fourElements.push(line[i]);
+  //   if (fourElements.length > 4) {
+  //     fourElements.shift();
+  //   }
+  //   if (fourElements.length === 4 && isUnique(fourElements)) {
+  //     console.log(i + 1);
+  //     break;
+  //   }
+  // }
 });
 
 rl.on("close", () => {
